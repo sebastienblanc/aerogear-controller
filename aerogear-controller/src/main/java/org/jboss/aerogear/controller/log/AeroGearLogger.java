d6169434122c8306064d8051fdc51491dcf2ea73
Douglas Campos <qmx@qmx.me>
2012-07-04 23:55:50 -0300

    adding jboss-logging

---
+++ aerogear-controller/src/main/java/org/jboss/aerogear/controller/log/AeroGearLogger.java
@@ -0,0 +1,31 @@
+package org.jboss.aerogear.controller.log;
+
+import org.jboss.aerogear.controller.RequestMethod;
+import org.jboss.logging.BasicLogger;
+import org.jboss.logging.LogMessage;
+import org.jboss.logging.Logger;
+import org.jboss.logging.Message;
+import org.jboss.logging.MessageLogger;
+
+import javax.servlet.ServletException;
+
+@MessageLogger(projectCode = "AG_CONTROLLER")
+public interface AeroGearLogger extends BasicLogger {
+    AeroGearLogger LOGGER = Logger.getMessageLogger(AeroGearLogger.class, AeroGearLogger.class.getPackage().getName());
+
+    @LogMessage(level = Logger.Level.FATAL)
+    @Message(id = 1, value = "must be run inside a Servlet container")
+    ServletException mustRunInsideAContainer();
+
+    @LogMessage(level = Logger.Level.ERROR)
+    @Message(id = 2, value = "oops, multivalued params not supported yet")
+    void multivaluedParamsUnsupported();
+
+    @LogMessage(level = Logger.Level.TRACE)
+    @Message(id = 3, value = "method: '%s', requested URI: '%s'")
+    void requestedRoute(RequestMethod method, String requestURI);
+
+    @LogMessage(level = Logger.Level.ERROR)
+    @Message(id = 4, value = "method: '%s', requested URI: '%s'")
+    RuntimeException routeNotFound(RequestMethod method, String requestURI);
+}
