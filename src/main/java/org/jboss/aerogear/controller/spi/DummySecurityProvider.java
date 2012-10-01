a87957f6c54be91d66122e387e4dee8988e5b139
Bruno Oliveira <bruno@abstractj.org>
2012-10-01 18:47:01 -0300

    Dummy bean must be alternative to avoid ambiguity

--- src/main/java/org/jboss/aerogear/controller/spi/DummySecurityProvider.java
+++ src/main/java/org/jboss/aerogear/controller/spi/DummySecurityProvider.java
@@ -2,6 +2,9 @@ package org.jboss.aerogear.controller.spi;
 
 import org.jboss.aerogear.controller.router.Route;
 
+import javax.enterprise.inject.Alternative;
+
+@Alternative
 public class DummySecurityProvider implements SecurityProvider {
     @Override
     public boolean isRouteAllowed(Route route) {
