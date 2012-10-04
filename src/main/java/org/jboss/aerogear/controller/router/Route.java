37a0de34fd4f4b9c9796cb80fa8cece65516d619
Daniel Bevenius <daniel.bevenius@gmail.com>
2012-10-04 15:16:44 +0200

    Adding exceptions support for Route interface.

--- src/main/java/org/jboss/aerogear/controller/router/Route.java
+++ src/main/java/org/jboss/aerogear/controller/router/Route.java
@@ -5,21 +5,81 @@ import org.jboss.aerogear.controller.RequestMethod;
 import java.lang.reflect.Method;
 import java.util.Set;
 
+/**
+ * An object that contains information required to route HTTP requests to a target class's method.
+ */
 public interface Route {
 
-    public Set<RequestMethod> getMethods();
+    /**
+     * Gets the {@link RequestMethod}s for this Route.
+     * 
+     * @return the {@link RequestMethod}s, or an empty set.
+     */
+    Set<RequestMethod> getMethods();
 
-    public String getPath();
+    /**
+     * Gets the path for this Route.
+     * 
+     * @return the path for this route, or null if no path exists for this Route.
+     */
+    String getPath();
 
+    /**
+     * Gets the {@link Method} of the target class for this Route.
+     * 
+     * @return the target method.
+     */
     Method getTargetMethod();
 
+    /**
+     * Gets the target class for this Route
+     * 
+     * @return the target class.
+     */
     Class<?> getTargetClass();
 
+    /**
+     * Determines if this Route can handle the {@link RequestMethod} and path combination.
+     * 
+     * @param method the http request methods.
+     * @param path the request path.
+     * @return {@code true} if this Route can handle the method and path passed in, {@code false} otherwise.
+     */
     boolean matches(RequestMethod method, String path);
 
+    /**
+     * Determines if this Route's path is parameterized.
+     * 
+     * @return {@code true} if this Route's path parameterized, otherwise {@code false}.
+     */
     boolean isParameterized();
 
+    /**
+     * Determines if this Route has roles configured.
+     * 
+     * @return {@code true} if this Route has roles associated with its Route.
+     */
     boolean isSecured();
 
-    String[] getRoles();
+    /**
+     * Gets this Routes associated roles.
+     * 
+     * @return the roles associated with this Route, or an empty set if there are no roles associated.
+     */
+    Set<String> getRoles();
+    
+    /**
+     * Determines if this Route contains one or more exception routes.
+     * 
+     * @return {@code true} if this Route has one or more exception routes.
+     */
+    boolean hasExceptionsRoutes();
+    
+    /**
+     * Determines if this Route can handle the throwable.
+     * 
+     * @param throwable 
+     * @return {@code true} if this Route can handle the Throwable, otherwise {@code false}.
+     */
+    public boolean canHandle(Throwable throwable);
 }
--- src/test/java/org/jboss/aerogear/controller/router/DefaultRouteTest.java
+++ src/test/java/org/jboss/aerogear/controller/router/DefaultRouteTest.java
@@ -1,28 +1,115 @@
 package org.jboss.aerogear.controller.router;
 
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.fest.assertions.Fail;
 import org.jboss.aerogear.controller.RequestMethod;
 import org.jboss.aerogear.controller.SampleController;
 import org.junit.Test;
 
-import java.lang.reflect.Method;
-
-import static org.fest.assertions.Assertions.assertThat;
-import static org.jboss.aerogear.controller.RequestMethod.GET;
-
 public class DefaultRouteTest {
+    
+    private static final RequestMethod[] GET = new RequestMethod[]{RequestMethod.GET};
+    private static final Class<?> TARGET_CLASS = SampleController.class;
+    private static final Method TARGET_METHOD = indexMethod(TARGET_CLASS, "index");
+    
+    @Test
+    public void construct() throws Exception {
+        final Route route = new DefaultRoute("/car/{id}", GET, TARGET_CLASS, TARGET_METHOD);
+        assertThat(route.getPath()).isEqualTo("/car/{id}");
+        assertThat(route.getMethods().size()).isEqualTo(1);
+        assertThat(route.getMethods()).contains(RequestMethod.GET);
+        assertThat(route.getTargetClass()).isEqualTo(SampleController.class);
+        assertThat(route.getTargetMethod()).isEqualTo(TARGET_METHOD);
+        assertThat(route.getRoles()).isEmpty();
+        assertThat(route.isSecured()).isFalse();
+        assertThat(route.hasExceptionsRoutes()).isFalse();
+    }
+    
+    @Test
+    public void createRouteWithNullHttpMethods() throws Exception {
+        final Route route = new DefaultRoute("/car/{id}", null, TARGET_CLASS, TARGET_METHOD);
+        assertThat(route.getMethods()).isEmpty();
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void constructWithNullTargetClass() throws Exception {
+        new DefaultRoute("/home", GET, null, TARGET_METHOD);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void constructWithNullTargetMethod() throws Exception {
+        new DefaultRoute("/home", GET, TARGET_CLASS, null);
+    }
+
+    @Test
+    public void constructWithNullRoles() throws Exception {
+        final Route route = new DefaultRoute("/car/{id}", GET, TARGET_CLASS, TARGET_METHOD, null);
+        assertThat(route.getRoles()).isEmpty();
+    }
+
+    @Test
+    public void constructWithNullExceptions() throws Exception {
+        final Route route = new DefaultRoute("/car/{id}", GET, TARGET_CLASS, TARGET_METHOD, new String[]{"admin"}, null);
+        assertThat(route.hasExceptionsRoutes()).isFalse();
+    }
+
+    @Test
+    public void constructWithRoles() throws Exception {
+        final Route route = new DefaultRoute("/car/{id}", GET, TARGET_CLASS, TARGET_METHOD, new String[] {"admin"});
+        assertThat(route.getRoles().size()).isEqualTo(1);
+        assertThat(route.getRoles()).contains("admin");
+    }
+    
+    @Test
+    public void constructWithExceptions() throws Exception {
+        @SuppressWarnings("unchecked")
+        final Set<Class<? extends Throwable>> exceptions = exceptions(IllegalArgumentException.class);
+        final Route route = new DefaultRoute("/car/{id}", GET, TARGET_CLASS, TARGET_METHOD, null, exceptions);
+        assertThat(route.hasExceptionsRoutes()).isTrue();
+    }
+    
+    @Test (expected = UnsupportedOperationException.class)
+    public void tryToModifyRoles() throws Exception {
+        final Route route = new DefaultRoute("/car/{id}", GET, TARGET_CLASS, TARGET_METHOD, new String[] {"admin"});
+        route.getRoles().remove("admin");
+    }
+
+    @Test (expected = UnsupportedOperationException.class)
+    public void tryToModifyHttpMethods() throws Exception {
+        final Route route = new DefaultRoute("/car/{id}", GET, TARGET_CLASS, TARGET_METHOD);
+        route.getMethods().remove(RequestMethod.GET);
+    }
 
     @Test
     public void matchesUnparameterizedRoutePaths() throws NoSuchMethodException {
-        final Method targetMethod = SampleController.class.getDeclaredMethod("index");
-        final Route route = new DefaultRoute("/index", new RequestMethod[]{GET}, SampleController.class, targetMethod);
-        assertThat(route.matches(GET, "/index")).isTrue();
+        final Route route = new DefaultRoute("/index", GET, TARGET_CLASS, TARGET_METHOD);
+        assertThat(route.matches(RequestMethod.GET, "/index")).isTrue();
     }
 
     @Test
     public void matchesParameterizedRoutePaths() throws NoSuchMethodException {
-        final Method targetMethod = SampleController.class.getDeclaredMethod("find", String.class);
-        final Route route = new DefaultRoute("/car/{id}", new RequestMethod[]{GET}, SampleController.class, targetMethod);
-        assertThat(route.matches(GET, "/car/3")).isTrue();
+        final Route route = new DefaultRoute("/car/{id}", GET, TARGET_CLASS, TARGET_METHOD);
+        assertThat(route.matches(RequestMethod.GET, "/car/3")).isTrue();
     }
 
+    private static final Method indexMethod(final Class<?> targetClass, final String methodName) {
+        Method m = null;
+        try {
+            m = targetClass.getDeclaredMethod(methodName);
+        } catch (final Exception e) {
+            Fail.fail("Could not find a method named '" + methodName + "' on target class", e);
+        }
+        return m;
+    }
+    
+    private Set<Class<? extends Throwable>> exceptions(final Class<? extends Throwable>... exs) {
+        return new HashSet<Class<? extends Throwable>>(Arrays.asList(exs));
+    }
+    
 }
